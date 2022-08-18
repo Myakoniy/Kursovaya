605eab497313f917270080923db7f059967b1504
ioann <yova@mail.ru>
2022-08-18 08:24:17 +0300

    Курсовая счетчик

---
+++ src/Employee.java
@@ -0,0 +1,38 @@
+public class Employee {
+
+    String fullName;
+    private int otdel;
+    private int zarplata;
+    private static int id = 0;
+
+    public Employee(int id, String fullName, int otdel, int zarplata) {
+        this.fullName = fullName;
+        this.otdel = otdel;
+        this.zarplata = zarplata;
+        this.id = id++;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getOtdel() {
+        return otdel;
+    }
+
+    public int getZarplata() {
+        return zarplata;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public void setOtdel(int otdel) {
+        this.otdel = otdel;
+    }
+
+    public void setZarplata(int zarplata) {
+        this.zarplata = zarplata;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+
+    Employee[] Sotrudniki = new Employee [10];
+
     public static void main(String[] args) {
+
+        Employee sotrudnik1 = new Employee("Сарманов Пётр Васильевич ", 3, 25000);
+        System.out.println(sotrudnik1.getFullName()+sotrudnik1.getOtdel()+sotrudnik1.getZarplata());
 
     }
 }
